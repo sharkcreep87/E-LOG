35d1112f4085992803a965c0f4c788f58fd705f5
sharkcreep87 <shafeq.hasan@gmail.com>
2014-11-04 10:30:24 +0800

    add UI in login page

--- application/views/login.php
+++ application/views/login.php
@@ -3,95 +3,82 @@
 <html lang="en">
   <head>
     
-    <link rel="icon" href="<?php echo base_url();?>images/Lock1.ico">
-    <title>Log In</title>
-	<link href="<?php echo base_url();?>css/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>css/css/bootstrap.min.css" rel="stylesheet">
-<style>
-
-body {
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #eee;
-}
-
-.form-signin {
-  max-width: 330px;
-  padding: 15px;
-  margin: 0 auto;
-}
-.form-signin .form-signin-heading,
-.form-signin .checkbox {
-  margin-bottom: 10px;
-}
-.form-signin .checkbox {
-  font-weight: normal;
-}
-.form-signin .form-control {
-  position: relative;
-  height: auto;
-  -webkit-box-sizing: border-box;
-     -moz-box-sizing: border-box;
-          box-sizing: border-box;
-  padding: 10px;
-  font-size: 16px;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type="username"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-.form-signin input[type="password"] {
-  margin-bottom: 0px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-
-</style>
+   
+    <title>Log Masuk</title>
 	
- <script src="<?php echo base_url();?>css/js/jquery.min.js"></script>
-    <script src="<?php echo base_url();?>css/js/bootstrap.min.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap-dropdown.js"></script>
+	
+	
+	 <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url();?>css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>css/style-responsive.css" rel="stylesheet" />
+
   </head>
 
   <body>
 
-   <div class="row">
-		<div class="col-md-4 col-md-offset-4">
-<div class="panel panel-default">
-			  	<div class="panel-heading">
-			    	<h3 class="panel-title">Please Log in</h3>
-			 	</div>
-			  	<div class="panel-body">
+ <body class="login-body">
+
+    <div class="container">
+
       <form class="form-signin" role="form"  method="post" action="<?php  $this->load->helper('url'); echo site_url("/login/process"); ?>">
-        
-        
+        <h2 class="form-signin-heading">LOG MASUK</h2>
+        <div class="login-wrap">
 		<?php if(! is_null($msg)) echo $msg;?>  
-		<div style="margin-bottom: 25px" class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input class="form-control" type="text" class="form-control" id ="UID" name ="UID" placeholder="Username" required autofocus>                                        
-                                    </div>
-									
-									<div style="margin-bottom: 25px" class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                        <input type="password" class="form-control" id ="Password" name ="Password" placeholder="Password" required>
-                                    </div>
-		
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-		<hr></hr>
-     <center> <p> <a href="<?php  $this->load->helper('url'); echo site_url("/creg"); ?>" >Click Here</a> for First Time Login</p></center>
-	  </form>
+            <input type="text" class="form-control" id ="UID" name ="UID" placeholder="Username" autofocus>
+            <input type="password" class="form-control" id ="Password" name ="Password" placeholder="Password"  required>
+          
+            <button class="btn btn-lg btn-login btn-block" type="submit">Sign in</button>
+          
+            <div class="registration">
+                DAFTAR AKAUN BARU?
+                <a class="" href="<?php  $this->load->helper('url'); echo site_url("/creg"); ?>">
+                    DAFTAR
+                </a>
+            </div>
+
+        </div>
+
+          <!-- Modal -->
+          <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                          <h4 class="modal-title">Forgot Password ?</h4>
+                      </div>
+                      <div class="modal-body">
+                          <p>Enter your e-mail address below to reset your password.</p>
+                          <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                      </div>
+                      <div class="modal-footer">
+                          <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                          <button class="btn btn-success" type="button">Submit</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!-- modal -->
+
+      </form>
+
+    </div>
 
 
-    </div> <!-- /container -->
-</div> 
- </div> <!-- /container -->
-</div> 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
- 
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="<?php echo base_url();?>js/bootstrap.min.js"></script>
+
+
   </body>
+
+
+
+
 </html>
