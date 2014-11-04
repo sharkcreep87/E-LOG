4dbe0e70198a5f64896c4c098d9bdcb99a971a78
sharkcreep87 <shafeq.hasan@gmail.com>
2014-11-03 20:33:10 -0800

    new ui for course

--- application/views/vcourseinfo.php
+++ application/views/vcourseinfo.php
@@ -6,76 +6,8 @@ header("Pragma: no-cache");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    
-    <link rel="icon" href="<?php echo base_url();?>images/Lock1.ico">
-    <title>Course Information</title>
-	<link href="<?php echo base_url();?>css/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>css/css/bootstrap.min.css" rel="stylesheet">
-<style>
-
-body {
- 
-  background-color: #eee;
-}
-
-
-.form-horizontal {
-
-  max-width: 630px;
-  padding: 25px;
-  margin: 0 auto;
-   background-color: #fff;
-}
-.form-control {
- 
-  margin-bottom: 10px;
-}
-  
-.navbar-collapse {
-        position: absolute;
-        left: 40%;
-        text-align: center;
-     top:15%;
-	  font-family: 'Georgia';
-    }
-	.navbar-brand-centered1 {
-        position: absolute;
-        left: 70%;
-		text-align: center;
-		top:0%;
-		 font-family: 'Arial';
-    }
-</style>
-	
- <script src="<?php echo base_url();?>css/js/jquery.min.js"></script>
-    <script src="<?php echo base_url();?>css/js/bootstrap.min.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap-dropdown.js"></script>
-  </head>
-
-  
-
-  <body >
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="collapse navbar-collapse">
-       
-    <h4> You have signed in as <b> <?php echo $this->session->userdata('UID'); ?> </b> </h4>
-        
-    </div>
-	<div class="navbar-brand navbar-brand-centered1">
-       
-    <b><a href="<?php  $this->load->helper('url'); echo site_url("/login/do_logout"); ?>"> <span class="glyphicon glyphicon-log-out"></span> LOGOUT</a></b>
-    
-        
-    </div>
-    </div> 
-
-	
-   <div class="jumbotron">
-   <div class="container">
+<?php include 'header.php';?>
+   
 
   <div class="row">
 		<div class="col-md-6 col-md-offset-3">
@@ -147,7 +79,6 @@ body {
       </div>
 	  </div>
 	 </div>
-      </div>
+     
 	  
-  </body>
-</html>
+ <?php include 'footer.php';?>
